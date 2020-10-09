a23ced3c6ab0bf4e4373a56ce8aa1b083f5f10a2
Pratyush-05 <profiesta98@gmail.com>
2020-10-09 13:51:07 +0530

    Uploading first Question

---
+++ BetweenTheOffices.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BetweenTheOffices {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        
+    }
+    public static int funct(){
+    }
+}
